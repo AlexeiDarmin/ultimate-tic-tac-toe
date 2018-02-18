5b15e1f7f742a56018fb9c201a0661bc9c5897e4
Alexei Darmin <alexei.darmin@gmail.com>
2018-02-18 17:41:29 -0500

    players can take turns

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,44 @@ import './App.css';
 import SmallBoard from './components/small_board';
 const logo = require('./logo.svg');
 
-class App extends React.Component {
+interface Props {
+
+}
+
+interface State {
+  board: Array<number>;
+  turn: number;
+}
+
+class App extends React.Component<Props , State> {
+
+  constructor(props: Props) {
+    super(props)
+
+    let defaultBoard = []
+    for (let i = 0; i < 81; ++i) {
+      defaultBoard.push(0)
+    }
+
+    this.state = {
+      board: defaultBoard,
+      turn: 1
+    }
+  }
+
+  handlePlayerMove = (cellIndex: number) => {
+    const { board, turn } = this.state
+
+    const newBoard = board.slice()
+    newBoard[cellIndex] = turn
+
+    const newTurn = turn === 1 ? 2 : 1;
+
+    this.setState({ board: newBoard, turn: newTurn })
+  }
+
   render() {
+    console.log('state:', this.state)
     return (
       <div className="App">
         <header className="App-header">
@@ -13,7 +49,9 @@ class App extends React.Component {
           <h1 className="App-title">Ultimate Tic Tac Toe</h1>
         </header>
         <div className="megaTictactoeBoard">
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((i) => <SmallBoard key={i} />)}
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(
+            (i) => <SmallBoard key={i} boardIndex={i} onClick={this.handlePlayerMove} board={this.state.board}/>
+          )}
         </div>
       </div>
     );
--- src/components/small_board.tsx
+++ src/components/small_board.tsx
@@ -1,12 +1,34 @@
 import * as React from 'react';
 import '../App.css';
 
-class SmallBoard extends React.Component {
+interface Props {
+  onClick: Function
+  boardIndex: number
+  board: Array<number>
+}
+class SmallBoard extends React.Component<Props> {
+
+  renderCells = () => {
+    const { onClick, boardIndex, board } = this.props
+
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8].map((i) => {
+      const cellIndex = boardIndex * 9 + i
+      console.log(board[cellIndex])
+      return (
+        <div
+          className="cell"
+          data-player={board[cellIndex]}
+          key={i}
+          onClick={() => board[cellIndex] === 0 ? onClick(cellIndex) : null}
+        />
+      )
+    })
+  }
 
   render() {
     return (
       <div className="tictactoeBoard">
-        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((i) => <div className="cell" key={i} />)}
+        {this.renderCells()}
       </div>
     );
   }
