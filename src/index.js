5cdf185de7e4429c07d0827d49423005db9d001a
AlexeiDarmin <alexei.darmin@gmail.com>
2018-02-21 20:37:34 -0500

    npm publish details

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import UltimateTTT from './App';
+import Game from './game'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<UltimateTTT />, document.getElementById('root'));
 registerServiceWorker();
+
+export {
+  Game,
+  UltimateTTT,
+}
