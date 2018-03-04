43823c6a5485840f356222e5ac017cc3a3498678
AlexeiDarmin <alexei.darmin@gmail.com>
2018-03-03 21:50:52 -0500

    running mtcs with ui, but not drawing 100% of matches are expected on a regular ttt grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import * as React from 'react';
 
-import { UltimateTicTacToe } from 'ultimate_tic_tic_toe';
+import { UltimateTicTacToe, SmallBoard as TicTacToe, runGame } from 'ultimate_tic_tic_toe';
 
+const board = runGame()
+const formattedBoard = [].concat.apply([], board.boardValues)
 class App extends React.Component {
+
   render() {
-    return <UltimateTicTacToe 
+    return <div>
+      <UltimateTicTacToe 
       playerOne="human" 
       playerTwo="bot_level_two" 
       loopGame={true} 
       moveDelay={50}
     />
+    <TicTacToe 
+      won={false}
+      boardIndex={0}
+      unlocked={true}
+      onClick={() => null}
+      board={formattedBoard}    
+    />
+    </div>
   }
 }
 
